d49f675fbfca1eb161fafa4fa3a7aa156b0035c8
Guilherme Freire <guitenan@hotmail.com>
2024-12-27 18:57:46 -0300

    Primeiro commit

---
+++ api/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Definir rotas no futuro
+module.exports = app;
